632004f8a9c9b3a06c19c438ea274d5ef60b0bc0
lipanpan03 <656461146@qq.com>
2023-09-27 16:13:52 +0800

    modify branchName

--- dist/index.js
+++ dist/index.js
@@ -9584,7 +9584,7 @@ async function getStarted() {
     try {
         const spaceId = `7800020`;
         const repo = process.env.GITHUB_REPOSITORY;
-        const branchName = process.env.GITHUB_REF_NAME;
+        const branchName = process.env.GITHUB_HEAD_REF;
         core.debug("branchName: " + branchName)
         const projectId = "9701567";
         const templateId = 9802227;
